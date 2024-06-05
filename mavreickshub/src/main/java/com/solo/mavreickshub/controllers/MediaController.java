4866392f45b10d620a87e5bee5e1535562e664da
banjoSolomon <ayomidebanjo68@gmail.com>
2024-06-05 15:07:45 +0100

    update

---
+++ mavreickshub/src/main/java/com/solo/mavreickshub/controllers/MediaController.java
@@ -0,0 +1,26 @@
+package com.solo.mavreickshub.controllers;
+
+import com.solo.mavreickshub.dtos.request.UpdateMediaRequest;
+import com.solo.mavreickshub.dtos.request.UploadMediaRequest;
+import com.solo.mavreickshub.services.MediaService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/v1/media")
+@AllArgsConstructor
+public class MediaController {
+    private final MediaService mediaService;
+@PostMapping(consumes= {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> uploadMedia(@ModelAttribute UploadMediaRequest uploadMediaRequest){
+    return ResponseEntity.status(CREATED).body(mediaService.upload(uploadMediaRequest));
+
+    }
+}
--- mavreickshub/src/test/java/com/solo/mavreickshub/controller/MediaControllerTest.java
+++ mavreickshub/src/test/java/com/solo/mavreickshub/controller/MediaControllerTest.java
@@ -1,15 +1,14 @@
 package com.solo.mavreickshub.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.solo.mavreickshub.dtos.request.UploadMediaRequest;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockPart;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,15 +17,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static com.solo.mavreickshub.utils.TestUtils.TEST_VIDEO_LOCATION;
-import static com.solo.mavreickshub.utils.TestUtils.buildUploadMediaRequest;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = {"/db/data.sql"})
 public class MediaControllerTest {
 
     @Autowired
