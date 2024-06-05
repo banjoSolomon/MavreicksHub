5138aacec2b16ad78c049d8254dfc213c77d2694
banjoSolomon <ayomidebanjo68@gmail.com>
2024-06-05 16:57:54 +0100

    update

--- mavreickshub/src/main/java/com/solo/mavreickshub/dtos/response/MediaResponse.java
+++ mavreickshub/src/main/java/com/solo/mavreickshub/dtos/response/MediaResponse.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.solo.mavreickshub.models.Category;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 @Getter
 @Setter
+@ToString
 public class MediaResponse {
     //SDLC
     private Long id;
@@ -19,5 +21,5 @@ public class MediaResponse {
     private LocalDateTime timeCreated;
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime timeUpdated;
-    private UserResponse user;
+    private UserResponse uploader;
 }
--- mavreickshub/src/main/java/com/solo/mavreickshub/dtos/response/UserResponse.java
+++ mavreickshub/src/main/java/com/solo/mavreickshub/dtos/response/UserResponse.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 @Getter
 @Setter
+@ToString
 public class UserResponse {
     private Long id;
     private String email;
