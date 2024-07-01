31e64bc5fb21d9aee9f631025b21c2b859cfedf2
banjoSolomon <ayomidebanjo68@gmail.com>
2024-07-01 14:43:34 +0100

    update

--- mavreickshub/src/main/java/com/solo/mavreickshub/security/filters/CustomAuthorizationFilter.java
+++ mavreickshub/src/main/java/com/solo/mavreickshub/security/filters/CustomAuthorizationFilter.java
@@ -1,9 +1,28 @@
 package com.solo.mavreickshub.security.filters;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.solo.mavreickshub.security.utils.SecurityUtils.PUBLIC_ENDPOINTS;
 
 @Component
-public class CustomAuthorizationFilter {
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       String requestPath = request.getServletPath();
+        boolean isRequestPathPublic = PUBLIC_ENDPOINTS.contains(requestPath);
+        if(isRequestPathPublic) filterChain.doFilter(request, response);
+       String authorizationHeader = request.getHeader("Authorization");
+
+
+
+    }
 }
---
+++ mavreickshub/src/main/java/com/solo/mavreickshub/security/utils/SecurityUtils.java
@@ -0,0 +1,8 @@
+package com.solo.mavreickshub.security.utils;
+
+import java.util.List;
+
+public class SecurityUtils {
+    private  SecurityUtils() {}
+    public static final List<String> PUBLIC_ENDPOINTS = List.of("/api/v1/auth");
+}
