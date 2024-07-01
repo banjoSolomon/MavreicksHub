6dbd424172b2695dfa47677dbe9394a0a4b3801f
banjoSolomon <ayomidebanjo68@gmail.com>
2024-07-01 12:17:24 +0100

    update

--- mavreickshub/src/main/java/com/solo/mavreickshub/security/config/SecurityConfig.java
+++ mavreickshub/src/main/java/com/solo/mavreickshub/security/config/SecurityConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @AllArgsConstructor
@@ -23,7 +23,9 @@ public class SecurityConfig {
         return http.csrf(c->c.disable())
                 .cors(c->c.disable())
                 .addFilterAt(authenticationFilter, BasicAuthenticationFilter.class)
-                .authorizeHttpRequests(c->c.anyRequest().permitAll())
+                .authorizeHttpRequests(c->c.requestMatchers(POST,"/api/v1/auth").permitAll()
+                        .requestMatchers("/api/v1/media").hasAuthority("USER"))
+
                 .build();
 
     }
---
+++ mavreickshub/src/main/java/com/solo/mavreickshub/security/filters/CustomAuthorizationFilter.java
@@ -0,0 +1,9 @@
+package com.solo.mavreickshub.security.filters;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthorizationFilter {
+
+
+}
